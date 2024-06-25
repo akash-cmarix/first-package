fb62f734cc6538bf8b12ef07f87721568f3010a1
akash.cmarix <akash.gundaniya@cmarix.com>
2024-06-25 12:58:07 +0530

    Initial commit

---
+++ src/cmarix/FirstPackage/FirstPackageServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace cmarix\Firstpackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class FirstpackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Bootstrap your package's services here.
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register your package's services here.
+    }
+}
